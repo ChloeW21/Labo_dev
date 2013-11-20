fc5250917c1b009b90f8b5a4ce5f83d569c88136
Maeils <silvio.vasc3@gmail.com>
2013-11-20 16:41:45 +0100

    test commit

---
+++ TFS/Consequence.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFS
+{
+    class Consequence
+    {
+        public Consequence(int id, int idDefi, int idUser, String nom, String texte)
+        {
+            this.id = id;
+            this.idDefi = idDefi;
+            this.idUser = idUser;
+            this.nom = nom;
+            this.texte = texte;
+        }
+
+        private int id;
+        private int idDefi;
+        private int idUser;
+        private String nom;
+        private String texte;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public int IdDefi
+        {
+            get { return idDefi; }
+            set { idDefi = value; }
+        }
+
+        public int IdUser
+        {
+            get { return idUser; }
+            set { idUser = value; }
+        }
+
+        public String Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+
+        public String Texte
+        {
+            get { return texte; }
+            set { texte = value; }
+        }
+    }
+}
---
+++ TFS/Utilisateur.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFS
+{
+    class Utilisateur
+    {
+        public Utilisateur(int id, String pseudo, String mdp, int score)
+        {
+            this.id = id;
+            this.pseudo = pseudo;
+            this.mdp = mdp;
+            this.score = score;
+        }
+
+        private int id;
+        private String pseudo;
+        private String mdp;
+        private int score;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public String Pseudo
+        {
+            get { return pseudo; }
+            set { pseudo = value; }
+        }
+
+        public String Mdp
+        {
+            get { return mdp; }
+            set { mdp = value; }
+        }
+
+        public int Score
+        {
+            get { return score; }
+            set { score = value; }
+        } 
+    }
+}
