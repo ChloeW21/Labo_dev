e7bd5eb6a15c23e14fe2b1df0d0f99fdad14fe8e
Millambeur <florian.chassot@gmail.com>
2013-12-18 11:22:13 +0100

    suppression écran inscription

--- AppliLabo/AppliLabo/Connexion.xaml.cs
+++ AppliLabo/AppliLabo/Connexion.xaml.cs
@@ -22,7 +22,8 @@ namespace AppliLabo
     /// </summary>
     public partial class Connexion : Page
     {
-        private MainWindow main; 
+        private MainWindow main;
+        private static int casError = 0;
 
         public Connexion(MainWindow main)
         {
@@ -40,7 +41,7 @@ namespace AppliLabo
             return maCommande.ExecuteScalar() != null;
         }
 
-        private void buttonValidate_Click(object sender, RoutedEventArgs e)
+        private void buttonConnexion_Click(object sender, RoutedEventArgs e)
         {
             if(Authentification(textBoxLogin.Text,passwordBoxMdp.Password)){
                 String email=main.getMailUtilisateur(textBoxLogin.Text);
@@ -51,9 +52,131 @@ namespace AppliLabo
                 textBoxLogin.Text="Authentification raté" ;
         }
 
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
         private void buttonInscription_Click(object sender, RoutedEventArgs e)
         {
-            main.frame.NavigationService.Navigate(new Inscription(main));
+            if (verifFormulaire())
+            {
+                MySqlCommand maCommande = main.getCommand();
+                string req = "Insert into `utilisateur` (`pseudo`, `mdp`, `score`,`email`) values (?pseudo ,?mdp,0,?email);";
+                maCommande.Parameters.AddWithValue("?pseudo", textBoxPseudo.Text);
+                maCommande.Parameters.AddWithValue("?mdp", passwordBoxMdp.Password);
+                maCommande.Parameters.AddWithValue("?email", textBoxEmail.Text);
+                maCommande.CommandText = req;
+                maCommande.ExecuteNonQuery();
+                UtilisateurSingleton.getInstance().setValues(textBoxPseudo.Text, textBoxEmail.Text, passwordBoxMdp.Password);
+                main.frame.NavigationService.Navigate(new Accueil(main));
+            }
+
+        }
+
+        public Boolean VerifLogin(string pseudo)
+        {
+            if (pseudo != "")
+            {
+                MySqlCommand maCommande = main.getCommand();
+                string req = "SELECT * FROM  `utilisateur` WHERE `pseudo` = ?pseudo";
+                maCommande.Parameters.AddWithValue("?pseudo", pseudo);
+                maCommande.CommandText = req;
+                if (maCommande.ExecuteScalar() != null)
+                {
+                    casError = 4;
+                    return false;
+                }
+                return true;
+            }
+            casError = 1;
+            return false;
+        }
+
+
+        public Boolean VerifMdp(string mdp, string mdpValidate)
+        {
+            if (mdp != "" && mdpValidate != "")
+            {
+                if (mdp != mdpValidate)
+                {
+                    casError = 5;
+                    return false;
+                }
+                return true;
+            }
+            casError = 2;
+            return false;
+        }
+
+
+        public Boolean VerifEmail(string email)
+        {
+            if (email != "")
+            {
+                MySqlCommand maCommande = main.getCommand();
+                string req = "SELECT * FROM  `utilisateur` WHERE `email` = ?email";
+                maCommande.Parameters.AddWithValue("?email", email);
+                maCommande.CommandText = req;
+                if (maCommande.ExecuteScalar() != null)
+                {
+                    casError = 6;
+                    return false;
+                }
+                return true;
+            }
+            casError = 3;
+            return false;
+        }
+
+        public Boolean verifFormulaire()
+        {
+            if (VerifLogin(textBoxPseudo.Text))
+                if (VerifEmail(textBoxEmail.Text))
+                    if (VerifMdp(passwordBoxMdp.Password, passwordBoxMdpValide.Password))
+                    {
+                        casError = 0;
+                        setErreur();
+                        return true;
+                    }
+            setErreur();
+            return false;
+        }
+
+
+        public void setErreur()
+        {
+            switch (casError)
+            {
+                case 0:
+                    textBlockError.Text = "";
+                    break;
+                case 1:
+                    textBlockError.Text = "Champ identifiant vide";
+                    textBoxPseudo.Focus();
+                    break;
+                case 2:
+                    textBlockError.Text = "Le ou les champs mot de passe sont vides";
+                    passwordBoxMdp.Focus();
+                    break;
+                case 3:
+                    textBlockError.Text = "Le champ email est vide";
+                    textBoxEmail.Focus();
+                    break;
+                case 4:
+                    textBlockError.Text = "Le login existe déjà dans la base de données";
+                    textBoxPseudo.Focus();
+                    break;
+                case 5:
+                    textBlockError.Text = "Les mots de passene correspondent pas";
+                    passwordBoxMdp.Focus();
+                    break;
+                case 6:
+                    textBlockError.Text = "L'email existe déjà dans la base de données";
+                    textBoxEmail.Focus();
+                    break;
+
+            }
         }
     }
 }
